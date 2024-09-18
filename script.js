8af80fb4163de97205d4fa41f7535deb87381618
spiderdog22 <na>
2024-09-18 18:33:43 -0500

    Draw a div grid with a script

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 let gridContainer = document.querySelector('.grid-container');
 
-const createBox = () => {
+const createBox = (width, height) => {
     let box = document.createElement('div');
+    box.style['width'] = width + 'px';
+    box.style['height'] = height + 'px';
     box.classList.add('gc-box');
     return box;
-}
+};
+
+const cleanGridContainer = () => {
+    let toRemove = [];
+
+    for (let element of gridContainer.childNodes)
+        toRemove.push(element);
+
+    for (let element of toRemove)
+        element.remove();
+};
 
 const setupGrid = (rows, cols) => {
+
+    cleanGridContainer();
+
+    let gridContainerWidth = gridContainer.clientWidth;
+    let gridContainerHeight = gridContainer.clientHeight;
+
+    let boxWidth = gridContainerWidth / rows;
+    let boxHeight = gridContainerHeight / cols;
+
+    console.log(boxWidth, boxHeight);
     
     for (let i = 0; i < rows * cols; i++) {
-        let box = createBox();
+        let box = createBox(boxWidth, boxHeight);
         gridContainer.appendChild(box);
     }
 
