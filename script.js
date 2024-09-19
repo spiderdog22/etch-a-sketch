eb7d5ffc471fd1e717773b243cb28ebf5a4c7ae0
spiderdog22 <na>
2024-09-18 21:33:45 -0500

    Add random color effect and fix darkening

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 let gridContainer = document.querySelector('.grid-container');
 
-gridContainer.addEventListener('mouseover', (event) => {
+const randCssRGB = () => {
+    let red = Math.trunc((Math.random() * 1000) % 256);
+    let green = Math.trunc((Math.random() * 1000) % 256);
+    let blue = Math.trunc((Math.random() * 1000) % 256);
+
+    return '' + red + ', ' + green + ', ' + blue;
+};
+
+const solidColor = (event) => {
     if (event.target.classList.contains('gc-box'))
     {
         let box = event.target;
-        let newOpacity = parseFloat(box.style.opacity) + 0.1;
-        box.style.opacity = '' + newOpacity;
+        box.style.opacity = '1.0';
     }
-});
+};
+
+const randomColorEffect = (event) => {
+    if (event.target.classList.contains('gc-box'))
+    {
+        let box = event.target;
+        box.style.backgroundColor = 'rgba(' + randCssRGB() + ', 1.0)';
+    }
+};
+
+const darkeningEffect = (event) => {
+    if (event.target.classList.contains('gc-box'))
+    {
+        let box = event.target;
+        let bcStyle = box.style['background-color'];
+
+        if (bcStyle.split(',').length < 4)
+            return;
+
+        let alpha = parseFloat(bcStyle.slice(bcStyle.lastIndexOf(',') + 1, bcStyle.lastIndexOf(')')));
+        let newRgba = 'rgba(0, 0, 0, ' + (alpha > 1.0 ? 1.0 : alpha + 0.1) + ')';
+        box.style['background-color'] = newRgba;
+    }
+};
+
+gridContainer.addEventListener('mouseover', darkeningEffect);
+// gridContainer.addEventListener('mousemove', randomColorEffect);
 
 const createBox = (width, height) => {
     let box = document.createElement('div');
     box.style['width'] = width + 'px';
     box.style['height'] = height + 'px';
-    box.style['opacity'] = '' + 0.0;
+    box.style['background-color'] = 'rgba(0, 0, 0, 0.0)';
     box.classList.add('gc-box');
     return box;
 };
